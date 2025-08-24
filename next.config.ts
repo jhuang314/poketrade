91c5bf1d511bae946331d36fe28dfe97e68a7b82
JH <jh3141@gmail.com>
2025-08-23 21:49:40 -0400

    brand new supabase template

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "raw.githubusercontent.com",
-        port: "",
-        pathname:
-          "/flibustier/pokemon-tcg-exchange/refs/heads/main/public/images/cards/**",
-      },
-    ],
-  },
+  /* config options here */
 };
 
 export default nextConfig;
--- src/components/ui/Button.tsx
+++
@@ -1,6 +0,0 @@
-// src/components/ui/Button.tsx
-import React from 'react';
-export const Button = React.forwardRef<HTMLButtonElement, React.ButtonHTMLAttributes<HTMLButtonElement>>(({ className, children, ...props }, ref) => {
-  return <button className={`w-full px-4 py-2 font-bold text-white bg-blue-500 rounded-md hover:bg-blue-600 disabled:bg-gray-400 ${className}`} ref={ref} {...props}>{children}</button>;
-});
-Button.displayName = "Button";
